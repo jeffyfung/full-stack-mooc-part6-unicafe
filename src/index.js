0a4b32d4d618a3a7b462004fb594bbea7b3c579e
Jeffy Fung <jeffyfungkh@gmail.com>
2022-05-07 18:03:09 +0800

    6.2: unicafe revisited, step2

--- src/index.js
+++ src/index.js
@@ -6,21 +6,23 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
+  const incrementCount = (feedback) => {
+    store.dispatch({ type: feedback })
+  }
+
+  const resetAllCounts = () => {
+    store.dispatch({ type: 'ZERO' })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => incrementCount('GOOD')}>good</button>
+      <button onClick={() => incrementCount('OK')}>ok</button>
+      <button onClick={() => incrementCount('BAD')}>bad</button>
+      <button onClick={resetAllCounts}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
