33575d4dd3574294f717119b0ec15c2af1e73d4b
Jeffy Fung <jeffyfungkh@gmail.com>
2022-05-07 17:56:59 +0800

    6.1 unicafe revisited, step1

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,5 +1,7 @@
 import deepFreeze from 'deep-freeze'
 import counterReducer from './reducer'
+import '@testing-library/jest-dom/extend-expect';
+
 
 describe('unicafe reducer', () => {
   const initialState = {
@@ -28,6 +30,55 @@ describe('unicafe reducer', () => {
     const newState = counterReducer(state, action)
     expect(newState).toEqual({
       good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('set zero', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = {
+      good: 3,
+      ok: 2,
+      bad: 1
+    }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
       ok: 0,
       bad: 0
     })
